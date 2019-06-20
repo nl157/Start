9c944b20ab9b02edb4b989166aa90c78faf436d4
Niall Lawrence <22602447+nl157@users.noreply.github.com>
2019-06-20 20:50:50 +0100

    Initial Commit. Added FizzBuzz game

---
+++ FizzBuzz/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to FizzBuzz \nType any number to start...\n");
+            int.TryParse(Console.ReadLine(), out int theNumber);
+            //TODO INPUT VALIDATION
+            for (int i = 1; i < theNumber + 1; i++)
+            {
+                String output = "";
+                bool isFizz = i % 3 == 0;
+                bool isBuzz = i % 5 == 0;
+
+                if (isFizz)
+                {
+                    output += "Fizz";
+                }
+                if (isBuzz)
+                {
+                    output += "Buzz";
+                }
+                if (output == "")
+                {
+                    output += i;
+                }
+
+                Console.WriteLine(output);
+            }
+
+        }
+    }
+}
